911d3ad7034e5856e715214c49259399f370408d
Aarthigv3023 <25bcs001.kgcas.com>
2025-09-10 18:10:22 +0530

    unit 2

---
+++ datatypes.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=50;
+	float b=2.34;
+	char c='A';
+	double d=2.34567;
+	printf(" thypes of data types \n");
+	printf("integer:%d\n",a);
+	printf("decimal:%f\n",b);
+	printf("character:%c\n",c);
+	printf("double:%.3f\n",d);
+	return 0;
+}
